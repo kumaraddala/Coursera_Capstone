f65ad25929eb822a21743bc6018b7c6a9782c86b
kumaraddala <63902966+kumaraddala@users.noreply.github.com>
2020-05-11 15:14:29 +0530

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone project
+
+# This notebook will be mainly used for the capstone project.
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print('Hello Capstone Project Course!')
+
